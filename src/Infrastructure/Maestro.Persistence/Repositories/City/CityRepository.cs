0572f601f43acd78fce19e7d53ae99b6266b1d38
LeventOzcelik89 <Levent.ozcelik@outlook.com>
2023-03-05 18:28:40 +0300

    City Repo

---
+++ src/Infrastructure/Maestro.Persistence/Repositories/City/CityRepository.cs
@@ -0,0 +1,16 @@
+﻿using Maestro.Application.Repository.City;
+using Maestro.Application.Repository.User;
+using Maestro.Domain.Entities;
+using Maestro.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maestro.Persistence.Repositories.City
+{
+
+    
+
+}
